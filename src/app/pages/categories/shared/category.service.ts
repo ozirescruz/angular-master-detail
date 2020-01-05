bfade064bb92bb19b4c2fb18d621c77559e6dcab
unknown <ozires.cruz@gmail.com>
2020-01-05 13:53:45 -0300

    adding category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+import { Category } from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiPath: string = "api/categories";
+
+  constructor(private http: HttpClient) { }
+  /*
+    getAll(): Observable<Category[]> {
+  
+    }
+  */
+}
