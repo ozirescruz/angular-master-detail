4441318b0eede4e86cce3cb2ba4c9566b710794f
Ozires da Cruz <ozires.cruz@gmail.com>
2020-01-10 17:16:26 -0300

    adding EntryListComponent

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,5 +1,6 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 import { Category } from './pages/categories/shared/category.model';
+import { Entry } from './pages/entries/shared/entry.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
@@ -8,10 +9,19 @@ export class InMemoryDatabase implements InMemoryDbService {
             { id: 2, name: "Saúde", description: "Plano de saúde" },
             { id: 3, name: "Lazer", description: "Cinema..." },
             { id: 4, name: "Salário", description: "Salário" },
-            { id: 5, name: "Freelas", description: "Freelancer" }
+            { id: 5, name: "Freelas", description: "Freelancer" },
+            { id: 6, name: "Esporte", description: "Esporte" },
         ];
 
-        return { categories };
+        const entries: Entry[] = [
+            { id: 1, name: 'Gás de cozinha', categoryId: categories[1].id, category: categories[1], paid: true, date: "01/01/2020", amount: "970,85", type: "expense", description: "Contas de casa" } as unknown as Entry,
+            { id: 2, name: 'Aluguel', categoryId: categories[2].id, category: categories[2], paid: false, date: "01/01/2020", amount: "666,80", type: "expense", description: "Contas de casa" } as unknown as Entry,
+            { id: 3, name: 'Plano de saúde', categoryId: categories[5].id, category: categories[5], paid: true, date: "01/01/2020", amount: "2.500,56", type: "revenue", description: "Contas de casa" } as unknown as Entry,
+            { id: 4, name: 'Comida', categoryId: categories[4].id, category: categories[4], paid: false, date: "01/01/2020", amount: "70,80", type: "expense", description: "Contas de casa" } as unknown as Entry,
+            { id: 5, name: 'Luz', categoryId: categories[3].id, category: categories[3], paid: true, date: "01/01/2020", amount: "120,52", type: "revenue", description: "Contas de casa" } as unknown as Entry
+        ];
+
+        return { categories, entries };
     }
 
 }
---
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { EntryService } from '../shared/entry.service';
+import { Entry } from '../shared/entry.model';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css']
+})
+export class EntryListComponent implements OnInit {
+
+  entries: Entry[] = [];
+
+  constructor(private entryService: EntryService) { }
+
+  ngOnInit() {
+    this.entryService.getAll().subscribe(entries => this.entries = entries,
+      error => console.log("ERRO NO GET ALL")
+    )
+
+  }
+
+  deleteEntry(entry) {
+    const mustDelete = confirm("Confirma exclusão?");
+
+    if (mustDelete) {
+      this.entryService.delete(entry.id).subscribe(
+        () => this.entries = this.entries.filter(element => element.id !== entry.id),
+        () => console.log("ERRO AO EXCLUIR ENTRY!")
+      )
+    }
+
+  }
+}
