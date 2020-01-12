6f2420c6b51743e2cabe274c02a7cc5242700a7c
Ozires da Cruz <ozires.cruz@gmail.com>
2020-01-12 15:28:20 -0300

    improving Entry list sorting

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,7 +1,7 @@
 import { Category } from '../../categories/shared/category.model';
 
 export class Entry {
-    constructor(public id?: Number,
+    constructor(public id?: number,
         public name?: string,
         public description?: string,
         public type?: string,
